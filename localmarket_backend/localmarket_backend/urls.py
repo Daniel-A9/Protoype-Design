6a795261543b0767cdbbe00c9a27b042c4b67725
Daniel-A9 <localmarketllc@proton.me>
2025-11-18 21:09:41 -0500

    Update backend settings, URLs, and ledger HTML templates.

--- localmarket_backend/localmarket_backend/urls.py
+++ localmarket_backend/localmarket_backend/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('ledger.urls', 'ledger'), namespace='ledger')),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
